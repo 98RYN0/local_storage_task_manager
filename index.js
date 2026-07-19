077b0b87b65a49bce4d688cded5ad34233d51bee
Ryan J. <ryanjay.dev@gmail.com>
2026-07-19 10:05:57 +1000

    added local storage functionality for persistence across refreshes

--- index.js
+++ index.js
@@ -3,37 +3,55 @@ const addTaskBtn = document.getElementById("add-task-btn")
 const taskContainer = document.getElementById("task-container")
 const taskList = document.getElementById("task-list")
 
-let tasksArr = []
+const storedData = localStorage.getItem('tasks')
+let taskArr = []
 let taskIdCounter = 0
+getStoredData()
+renderTasks()
+console.log(taskArr)
+
 
 addTaskBtn.addEventListener('click', function(){
-    if (taskInputEl.value){
+    if(taskInputEl.value){
         let newTask = {id: taskIdCounter++, text: taskInputEl.value, completed: false}
-        tasksArr.push(newTask)
-        taskInputEl.value = ''
-        renderTasks()
+            taskArr.push(newTask)
+            taskInputEl.value = ''
+            localStorage.setItem('task ' + newTask.id, JSON.stringify(newTask))
+            renderTasks()
     }
 })
 
 taskContainer.addEventListener('click', function(e){
     if (e.target.classList.contains("complete-task-btn")){
         let taskId = e.target.parentElement.id
-        tasksArr.forEach(task => {
+        taskArr.forEach(task => {
             if (task.id === Number(taskId)){task.completed = !task.completed}
         })
         renderTasks()
     }
     else if (e.target.classList.contains("delete-task-btn")){
         let taskId = e.target.parentElement.id
-        tasksArr = tasksArr.filter(task => task.id !== Number(taskId))
+        taskArr = taskArr.filter(task => task.id !== Number(taskId))
+        localStorage.removeItem('task '+ taskId)
+        taskIdCounter--
         renderTasks()
     }
 })
 
+function getStoredData() {
+    const keys = Object.keys(localStorage)
+    taskArr = keys.map(key => JSON.parse(localStorage.getItem(key)))
+    if (taskArr.length > 0) {taskIdCounter = taskArr.length}
+}
+
 function renderTasks() {
     let taskHtml = ''
-    taskHtml = tasksArr.map(task => {
-        return `<li class="task ${task.completed ? 'completed' : ''}" id="${task.id}">${task.text}<button class="complete-task-btn">Complete Task</button><button class="delete-task-btn">Delete Task</button></li>`
+    taskHtml = taskArr.map(task => {
+        return `
+        <li class="task ${task.completed ? 'completed' : ''}" id="${task.id}">${task.text}
+            <button class="complete-task-btn">Complete Task</button>
+            <button class="delete-task-btn">Delete Task</button>
+        </li>`
     }).join('')
     taskList.innerHTML = taskHtml
     if (taskHtml && taskList.classList.contains('hidden')){taskList.classList.toggle('hidden')}
